68c8aef52fa870b7b86abc7a8c84bcb518562801
Lordimass <lordimass@lordimass.net>
2026-05-31 13:42:46 +0100

    Init with AzureDoom Hytale Plugin

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    idea
+    java
+    id("com.azuredoom.hytale-tools") version "1.+"
+    id("com.azuredoom.hytalepublisher") version "1.+"
+}
+
+
+tasks.withType<Javadoc>().configureEach {
+    (options as org.gradle.external.javadoc.StandardJavadocDocletOptions).addStringOption("Xdoclint:-missing", "-quiet")
+}
+
+group = project.property("group").toString()
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(property("java_version").toString().toInt()))
+}
+
+hytaleTools {
+    javaVersion = property("java_version").toString().toInt()
+    hytaleVersion = property("hytale_version").toString()
+    manifestServerVersion = property("manifestServerVersion").toString()
+    manifestGroup = property("manifest_group").toString()
+    modId = property("mod_id").toString()
+    modDescription = property("mod_description").toString()
+    modUrl = property("mod_url").toString()
+    mainClass = property("main_class").toString()
+    modCredits = property("mod_author").toString()
+    manifestDependencies = property("manifest_dependencies").toString()
+    manifestOptionalDependencies = property("manifest_opt_dependencies").toString()
+    curseforgeId = property("curseforgeID").toString()
+    disabledByDefault = property("disabled_by_default").toString().toBoolean()
+    includesPack = property("includes_pack").toString().toBoolean()
+    patchline = property("patchline").toString()
+}
+
+repositories {
+    mavenCentral()
+}
+
+idea {
+    module {
+        isDownloadSources = true
+        isDownloadJavadoc = true
+    }
+}
+
+
+hytalePublisher {
+    curseforge {
+        enabled = true
+        projectId = property("curseforge_project_id").toString()
+    }
+}
