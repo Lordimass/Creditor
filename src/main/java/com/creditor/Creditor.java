90c27fe110251e720a3e116a655b89da2bb017d3
RM20 <43253883+rm20killer@users.noreply.github.com>
2026-06-05 00:16:14 +0100

    Prevent double registration; bump to 1.0.4
    prevent console spam when multiple mods are using creditor

--- src/main/java/com/creditor/Creditor.java
+++ src/main/java/com/creditor/Creditor.java
@@ -2,6 +2,9 @@ package com.creditor;
 
 import com.creditor.asset.CreditAsset;
 import com.creditor.command.CreditsCommand;
+import com.hypixel.hytale.assetstore.AssetRegistry;
+import com.hypixel.hytale.assetstore.map.JsonAssetWithMap;
+import com.hypixel.hytale.builtin.asseteditor.AssetEditorPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import lombok.Getter;
 
@@ -10,8 +13,14 @@ public class Creditor {
     private Creditor() {}
 
     public static void setup(JavaPlugin host) {
-        host.getAssetRegistry().register(CreditAsset.createAssetStore());
-        host.getCommandRegistry().registerCommand(new CreditsCommand());
+        if(AssetEditorPlugin.get().getAssetTypeRegistry().getAssetTypeHandler("CreditAsset")==null) {
+            AssetRegistry.register(CreditAsset.createAssetStore());
+            host.getCommandRegistry().registerCommand(new CreditsCommand());
+//          host.getLogger().atInfo().log("Creditor registered");
+        }
+//        else{
+//            host.getLogger().atInfo().log("Creditor has been set up by another plugin skipping registration");
+//        }
     }
 
     public static void start(JavaPlugin host) {}
