c2878a5b219ae70efeff614cd4ab7bd931266619
Lordimass <lordimass@lordimass.net>
2026-06-04 14:31:17 +0100

    Fixed version mismatch problem

--- src/main/java/com/creditor/Creditor.java
+++ src/main/java/com/creditor/Creditor.java
@@ -9,24 +9,10 @@ public class Creditor {
 
     private Creditor() {}
 
-    // Used to prevent mod from being initialised multiple times if more than one mod is installed which uses Creditor as a library.
-    private static boolean isSetupDone = false;
-    private static boolean isStartDone = false;
-
     public static void setup(JavaPlugin host) {
-        if (isSetupDone) return;
-
         host.getAssetRegistry().register(CreditAsset.createAssetStore());
         host.getCommandRegistry().registerCommand(new CreditsCommand());
-
-        isSetupDone = true;
     }
 
-    public static void start(JavaPlugin host) {
-        if (isStartDone) return;
-
-
-
-        isStartDone = true;
-    }
+    public static void start(JavaPlugin host) {}
 }
